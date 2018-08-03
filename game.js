722ab8ce262d1c8ba1f37ab1204c93a5aaeb3fb1
Tavia Sterling <TaviaSterling@outlook.com>
2018-08-02 21:30:27 -0600

    style updates

--- game.js
+++ game.js
@@ -1,15 +1,30 @@
 var health = 100;
+let targetName = "Creeper";
+let hits = 0;
 const slapButton = document.getElementById('slap-button')
 
 function slap() {
-        health--;   
+        health--;  
+        hits++; 
         alert(health)
         update();   
     }
 
+function punch() {
+    health -=5;
+    hits++;
+    update()
+}
+function kick() {
+    health -=10;
+    hits++;
+    update()
+}
+
 function update() {
  document.getElementById('health').innerText = health.toString()
-
+document.getElementById('hits').innerText = hits.toString()
+document.getElementById('targetName').innerText = targetName
 }
 
 update()
